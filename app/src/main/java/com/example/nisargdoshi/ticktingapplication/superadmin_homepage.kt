3b583f6cc92ff1a6310f1952d4b4588aaa7d6507
Nisarg Doshi <nisarg@visibly.io>
2019-01-17 00:06:29 +0530

    searchview in actionbar

--- app/src/main/java/com/example/nisargdoshi/ticktingapplication/superadmin_homepage.kt
+++ app/src/main/java/com/example/nisargdoshi/ticktingapplication/superadmin_homepage.kt
@@ -1,15 +1,20 @@
 package com.example.nisargdoshi.ticktingapplication
 
+import android.app.ActionBar
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.design.widget.NavigationView
 import android.support.v4.view.GravityCompat
+
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_superadmin_homepage.*
 import kotlinx.android.synthetic.main.app_bar_superadmin_homepage.*
+import android.view.Gravity
+import android.view.ViewGroup
+
 
 class superadmin_homepage : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
@@ -17,7 +22,9 @@ class superadmin_homepage : AppCompatActivity(), NavigationView.OnNavigationItem
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_superadmin_homepage)
         setSupportActionBar(toolbar)
+        setTitle("Home")
 
+        actionBar?.setTitle("Hohgvgh")
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
@@ -30,6 +37,10 @@ class superadmin_homepage : AppCompatActivity(), NavigationView.OnNavigationItem
         toggle.syncState()
 
         nav_view.setNavigationItemSelectedListener(this)
+
+
+        //sv_serchhome_superadminhome.setLayoutParams(supportActionBar.LayoutParams(Gravity.RIGHT))
+
     }
 
     override fun onBackPressed() {
@@ -40,41 +51,33 @@ class superadmin_homepage : AppCompatActivity(), NavigationView.OnNavigationItem
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.superadmin_homepage, menu)
-        return true
-    }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        when (item.itemId) {
-            R.id.action_settings -> return true
-            else -> return super.onOptionsItemSelected(item)
-        }
-    }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         // Handle navigation view item clicks here.
         when (item.itemId) {
-            R.id.nav_camera -> {
+            R.id.nav_organization -> {
                 // Handle the camera action
             }
-            R.id.nav_gallery -> {
+            R.id.nav_users -> {
 
             }
-            R.id.nav_slideshow -> {
+            R.id.nav_department -> {
 
             }
-            R.id.nav_manage -> {
+            R.id.nav_category-> {
 
             }
-            R.id.nav_share -> {
+            R.id.nav_aboutus -> {
 
             }
-            R.id.nav_send -> {
+            R.id.nav_setting-> {
+
+            }
+            R.id.nav_rateus->{
+
+            }
+            R.id.nav_logout->{
 
             }
         }
@@ -83,3 +86,12 @@ class superadmin_homepage : AppCompatActivity(), NavigationView.OnNavigationItem
         return true
     }
 }
+
+private fun Any?.LayoutParams(right: Int): ViewGroup.LayoutParams? {
+
+    return null;
+}
+
+
+
+
